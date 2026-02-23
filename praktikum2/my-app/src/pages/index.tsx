9688be2082f7da1cf0338dd1d8cdb35961e34584
dhanisaptrri <shadhanisa2@gmail.com>
2026-02-24 04:25:45 +0700

    Modifikasi index.tsx pada folder pages

--- praktikum2/my-app/src/pages/index.tsx
+++ praktikum2/my-app/src/pages/index.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import Navbar from "@/components/layouts/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Praktikum Next.js Pages Router</h1>
       <br />
       <p>Mahasiswa D4 Pengembangan Web</p>
