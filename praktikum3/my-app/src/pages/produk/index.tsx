5798cc050d4e51120b597d5bb733ea81f450f600
dhanisaptrri <shadhanisa2@gmail.com>
2026-02-24 23:42:09 +0700

    Tugas 1

--- praktikum3/my-app/src/pages/produk/index.tsx
+++ praktikum3/my-app/src/pages/produk/index.tsx
@@ -1,9 +1,16 @@
-const produk = () => {
-    return (
-        <div>
-            Produk User Page 
-        </div>
-    );
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const Produk = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, [isLogin, push]);
+
+  return <div>Produk User Page</div>;
 };
 
-export default produk;
+export default Produk;
