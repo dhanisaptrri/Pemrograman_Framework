192e0002452fe47a83b44da84bcb113a22c858c1
dhanisaptrri <shadhanisa2@gmail.com>
2026-02-28 16:34:16 +0700

    Modifikasi login.tsx

--- Jobsheet5/my-app/src/pages/auth/login.tsx
+++ Jobsheet5/my-app/src/pages/auth/login.tsx
@@ -1,19 +1,22 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from './login.module.css';
 
-const Login = () => {
-  const { push } = useRouter();
+const halamanLogin = () => {
+  const {push} = useRouter();
 
-  const handleLogin = () => {
-    localStorage.setItem("isLogin", "true");
-    push("/produk");
-  };
+  const handlerLogin = () => {
+    // logic login disini
+    push('/produk');
+  }
 
   return (
-    <div>
+    <div className={styles.login}>
       <h1>Halaman Login</h1>
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={() => handlerLogin()}>Login</button> <br />
+      <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
 };
 
-export default Login;
+export default halamanLogin;
