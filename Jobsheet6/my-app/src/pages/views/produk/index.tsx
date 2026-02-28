28590d2c11323a44987a87fc1e7b351bce77df3d
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-01 01:58:46 +0700

    Modifikasi pada folder pages _document.js

---
+++ Jobsheet6/my-app/src/pages/views/produk/index.tsx
@@ -0,0 +1,10 @@
+const HeroSection = () => {
+  return (
+    <section>
+      <h1>Halaman Produk</h1>
+      <p>Selamat datang di halaman produk</p>
+    </section>
+  );
+};
+
+export default HeroSection;
