a152307bbbe48dc456dc4d3fb84be4e05d874a3e
dhanisaptrri <shadhanisa2@gmail.com>
2026-02-28 22:33:01 +0700

    Tugas 1

--- Jobsheet5/my-app/src/pages/auth/register.tsx
+++ Jobsheet5/my-app/src/pages/auth/register.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import styles from "./register.module.css";
 
 const halamanRegister = () => {
-    return (
-        <div>
-            <h1>Halaman Register</h1>
-            <Link href="/auth/login">Ke Halaman Login</Link>
-        </div>
-    );
-}
+  return (
+    <div className={styles.register}>
+      <h1>Halaman Register</h1>
+      <Link href="/auth/login">Ke Halaman Login</Link>
+    </div>
+  );
+};
 
 export default halamanRegister;
