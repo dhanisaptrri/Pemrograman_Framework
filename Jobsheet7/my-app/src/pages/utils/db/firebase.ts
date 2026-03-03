1b7f22071f07e8d8d016ca1f06fb002289cf8bc4
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-03 21:49:27 +0700

    modifikasi file servicefirebase.js

--- Jobsheet7/my-app/src/pages/utils/db/firebase.ts
+++ Jobsheet7/my-app/src/pages/utils/db/firebase.ts
@@ -1,3 +1,5 @@
+import { initializeApp} from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC50QNQ54SxzLQ-X63M-PZSuQauNMblUes",
   authDomain: "framework-next-c7ed3.firebaseapp.com",
@@ -6,3 +8,6 @@ const firebaseConfig = {
   messagingSenderId: "886587621997",
   appId: "1:886587621997:web:559ec1bd30808b79613564"
 };
+
+const app = initializeApp(firebaseConfig);
+export default app;
