677ffca2a4675106635847904becd4220d4897e1
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-04 01:38:50 +0700

    Implementasi SWR

--- Jobsheet8/my-app/src/pages/views/product/index.tsx
+++ Jobsheet8/my-app/src/pages/views/product/index.tsx
@@ -1,48 +1,26 @@
-import styles from "../../produk/product.module.scss";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "../views/product";
+import useSWR from "swr";
 
-type ProductType = {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  category: string;
-};
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  // console.log("products:", products);
+
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+  //cek apakah data, error, dan isLoading sudah benar
+  // console.log("data:", data);
+  // console.log("error:", error);
+  // console.log("isLoading:", isLoading);
+
   return (
-    <div className={styles.produk}>
-      <h1 className={styles.produk__title}>Daftar Produk</h1>
-      <div className={styles.produk__content}>
-        {products.length > 0 ? (
-          <>
-            {products.map((products: ProductType) => (
-              <div key={products.id} className={styles.produk__content__item}>
-                <div className={styles.produk__content__item__image}>
-                  <img src={products.image} alt={products.name} width={200} />
-                </div>
-                <h4 className={styles.produk__content__item__name}>
-                  {products.name}
-                </h4>
-                <p className={styles.produk__content__item__category}>
-                  {products.category}
-                </p>
-                <p className={styles.produk__content__item__price}>
-                  Rp {products.price.toLocaleString()}
-                </p>
-              </div>
-            ))}
-          </>
-        ) : (
-          <div className={styles.produk__content__skeleton}>
-            <div className={styles.produk__content__skeleton__image}></div>
-            <div className={styles.produk__content__skeleton__name}></div>
-            <div className={styles.produk__content__skeleton__category}></div>
-            <div className={styles.produk__content__skeleton__price}></div>
-          </div>
-        )}
-      </div>
+    <div>
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
   );
 };
 
-export default TampilanProduk;
+export default kategori;
