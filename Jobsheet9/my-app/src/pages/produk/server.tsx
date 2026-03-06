3e1a4152ea299091a7f2d41eb9882f7ef5a335d7
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-06 22:31:57 +0700

    Modifikasi file server.tsx :

---
+++ Jobsheet9/my-app/src/pages/produk/server.tsx
@@ -0,0 +1,13 @@
+import TampilanProduk from "../views/produk";
+
+const halamanProdukServer = () => {
+    return (
+        <div>
+            <h1>Halaman Produk Server</h1>
+            <TampilanProduk products={[]}/>
+        </div>
+        
+    );
+};
+
+export default halamanProdukServer;
