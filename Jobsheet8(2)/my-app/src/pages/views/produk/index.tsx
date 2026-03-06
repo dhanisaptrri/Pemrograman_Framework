47640797bdfdb76161c50ceb64eead82d25f73bd
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-06 21:45:21 +0700

    Modfikasi file index.tsx pada folder views/product/index.tsx

--- Jobsheet8(2)/my-app/src/pages/views/produk/index.tsx
+++ Jobsheet8(2)/my-app/src/pages/views/produk/index.tsx
@@ -13,7 +13,7 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
       <div className={styles.produk__content}>
-        {products.map((products: ProductType) => (
+        {/* {products.map((products: ProductType) => (
           <div key={products.id} className={styles.produk__content__item}>
             <div className={styles.produk__content__item__image}>
               <img src={products.image} alt={products.name} width={200} />
@@ -28,7 +28,15 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
               Rp {products.price.toLocaleString()}
             </p>
           </div>
-        ))}
+        ))} 
+        */}
+
+        <div className={styles.produk__content__skeleton}>
+          <div className={styles.produk__content__skeleton__image}></div>
+          <div className={styles.produk__content__skeleton__name}></div>
+          <div className={styles.produk__content__skeleton__category}></div>
+          <div className={styles.produk__content__skeleton__price}></div>
+        </div>
       </div>
     </div>
   );
