b9cd159957e6274b12faef957c411c19ddfe30df
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-07 22:24:31 +0700

    js9

---
+++ Jobsheet10/my-app/src/components/layouts/navbar/index.tsx
@@ -0,0 +1,13 @@
+import syles from './navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div className={syles.navbar}>
+      <div className='big'>
+        <h1>Navbar Component</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ Jobsheet10/my-app/src/pages/utils/db/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC50QNQ54SxzLQ-X63M-PZSuQauNMblUes",
+  authDomain: "framework-next-c7ed3.firebaseapp.com",
+  projectId: "framework-next-c7ed3",
+  storageBucket: "framework-next-c7ed3.firebasestorage.app",
+  messagingSenderId: "886587621997",
+  appId: "1:886587621997:web:559ec1bd30808b79613564"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
