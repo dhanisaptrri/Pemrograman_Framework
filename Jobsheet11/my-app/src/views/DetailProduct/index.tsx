3991682bee7a205900a249ca0582960a930a1f08
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-10 19:03:27 +0700

    Modifikasi index.tsx pada folder DetailProduct

---
+++ Jobsheet11/my-app/src/views/DetailProduct/index.tsx
@@ -0,0 +1,23 @@
+// import { ProductType } from "@/types/Product.type";
+import { ProductType } from "../../types/Product.type";
+import styles from "../DetailProduct/detailProduct.module.scss";
+
+const DetailProduk = ({ products }: { products: ProductType }) => {
+  return (
+    <div className={styles.produkdetail}>
+      <div className={styles.produkdetail__image}>
+        <img src={products.image} alt={products.name} />
+      </div>
+
+      <div className={styles.produkdetail__info}>
+        <h1 className={styles.produkdetail__name}>{products.name}</h1>
+        <p className={styles.produkdetail__category}>{products.category}</p>
+        <p className={styles.produkdetail__price}>
+          Rp {products.price.toLocaleString("id-ID")}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DetailProduk;
