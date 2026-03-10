9985018f0ddeafe6f9162cc2a6fb41561a69442b
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-10 19:09:40 +0700

    Modifikasi index.tsx pada folder views/detailProduct line 16

--- Jobsheet11/my-app/src/views/DetailProduct/index.tsx
+++ Jobsheet11/my-app/src/views/DetailProduct/index.tsx
@@ -4,19 +4,22 @@ import styles from "../DetailProduct/detailProduct.module.scss";
 
 const DetailProduk = ({ products }: { products: ProductType }) => {
   return (
-    <div className={styles.produkdetail}>
-      <div className={styles.produkdetail__image}>
-        <img src={products.image} alt={products.name} />
-      </div>
+    <>
+      <h1>Detail Produk</h1>
+      <div className={styles.produkdetail}>
+        <div className={styles.produkdetail__image}>
+          <img src={products.image} alt={products.name} />
+        </div>
 
-      <div className={styles.produkdetail__info}>
-        <h1 className={styles.produkdetail__name}>{products.name}</h1>
-        <p className={styles.produkdetail__category}>{products.category}</p>
-        <p className={styles.produkdetail__price}>
-          Rp {products.price.toLocaleString("id-ID")}
-        </p>
+        <div className={styles.produkdetail__info}>
+          <h1 className={styles.produkdetail__name}>{products.name}</h1>
+          <p className={styles.produkdetail__category}>{products.category}</p>
+          <p className={styles.produkdetail__price}>
+            Rp {products.price && products.price.toLocaleString("id-ID")}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
