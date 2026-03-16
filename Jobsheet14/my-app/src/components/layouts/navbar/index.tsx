89654d87e47595018f37a5bad7806632dab14b2e
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-16 20:00:57 +0700

    Update Navbar component to conditionally render Sign in/Sign out buttons based on session data

--- Jobsheet14/my-app/src/components/layouts/navbar/index.tsx
+++ Jobsheet14/my-app/src/components/layouts/navbar/index.tsx
@@ -1,12 +1,19 @@
 import syles from './navbar.module.css';
 import { signIn, signOut, useSession } from 'next-auth/react';
 const Navbar = () => {
+  const {data} = useSession()
+  //const { data: session } = useSession(); 
+  //console.log("session", session);
   return (
     <div className={syles.navbar}>
       <div className='big'>
         <h1>Navbar Component</h1>
       </div>
+      {data ?(
       <button onClick={() => signIn()}>Sign in</button>
+      ) : (
+        <button onClick={() => signOut()}>Sign out</button>
+      )}
     </div>
   );
 };
