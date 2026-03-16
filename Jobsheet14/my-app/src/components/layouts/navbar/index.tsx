ea0804311a0ca11f6c5aacb560694ae262a10038
dhanisaptrri <shadhanisa2@gmail.com>
2026-03-16 19:46:23 +0700

    Bagian 5 – Tambahkan Tombol Login & Logout

--- Jobsheet14/my-app/src/components/layouts/navbar/index.tsx
+++ Jobsheet14/my-app/src/components/layouts/navbar/index.tsx
@@ -1,11 +1,12 @@
 import syles from './navbar.module.css';
-
+import { signIn, signOut, useSession } from 'next-auth/react';
 const Navbar = () => {
   return (
     <div className={syles.navbar}>
       <div className='big'>
         <h1>Navbar Component</h1>
       </div>
+      <button onClick={() => signIn()}>Sign in</button>
     </div>
   );
 };
