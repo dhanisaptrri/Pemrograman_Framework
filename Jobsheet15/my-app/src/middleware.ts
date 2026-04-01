82a97d777d9649d5f08cbc4f26c6b3ee68c28ef8
dhanisaptrri <shadhanisa2@gmail.com>
2026-04-01 17:53:43 +0700

    update salah js14

--- Jobsheet15/my-app/src/middleware.ts
+++ Jobsheet15/my-app/src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
-import withAuth from "./Middleware/withAuth"
+import withAuth from "./middleware/withAuth"
 
 function middleware(request: NextRequest) {
     return NextResponse.next()
 }
+
 export default withAuth(middleware, ["/profile"])
 
 export const config = {
