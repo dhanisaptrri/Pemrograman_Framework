f4953d7481359e878ed4d73d949d5e8809bf5d60
dhanisaptrri <shadhanisa2@gmail.com>
2026-04-06 14:07:38 +0700

    Gunakan next/image untuk optimasi avatar

--- Jobsheet17/my-app/next.config.js
+++ Jobsheet17/my-app/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Domain Foto Google
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com', // Domain Foto GitHub
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
